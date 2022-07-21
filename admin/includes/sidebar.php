82572710a805b1e336f2af25424ed1a06800edab
karthikacb <78253352+karthikacb@users.noreply.github.com>
2022-07-21 15:29:55 +0530

    Add files via upload

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,127 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+          <ul class="nav">
+            <li class="nav-item nav-profile">
+              <a href="#" class="nav-link">
+                <div class="profile-image">
+                  <img class="img-xs rounded-circle" src="images/faces/face8.jpg" alt="profile image">
+                  <div class="dot-indicator bg-success"></div>
+                </div>
+                <div class="text-wrapper">
+                  
+                  <p class="profile-name"></p>
+                  <p class="designation">
+                </div>
+               
+              </a>
+            </li>
+            <li class="nav-item nav-category">
+              <span class="nav-link">Dashboard</span>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="dashboard.php">
+                <span class="menu-title">Dashboard</span>
+                <i class="icon-screen-desktop menu-icon"></i>
+              </a>
+            </li>
+            
+            <!-- <li class="nav-item">
+              <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <span class="menu-title">User-Approval</span>
+                <i class="icon-layers menu-icon"></i>
+              </a>
+              <div class="collapse" id="ui-basic">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="userapproval.php">Approval</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="approveduser.php">Approved Users</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="rejecteduser.php">Rejected Users</a></li>
+                </ul>
+              </div>
+            </li> -->
+            <li class="nav-item">
+              <a class="nav-link" href="totalusers.php">
+                <span class="menu-title">Total Users</span>
+                <i class="icon-people menu-icon"></i>
+              </a>
+            </li>
+            <!-- <li class="nav-item">
+              <a class="nav-link" href="viewrequest.php">
+                <span class="menu-title">View Request</span>
+                <i class="icon-people menu-icon"></i>
+              </a>
+            </li> -->
+            <li class="nav-item">
+              <a class="nav-link" href="all-approvedusers.php">
+                <span class="menu-title">View user details</span>
+                <i class="icon-people menu-icon"></i>
+              </a>
+            </li>
+            <!-- <li class="nav-item">
+              <a class="nav-link" href="assigneduser.php">
+                <span class="menu-title">Dietitian Assigned Users</span>
+                <i class="icon-people menu-icon"></i>
+              </a>
+            </li> -->
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="collapse" href="#ui-basic1" aria-expanded="false" aria-controls="ui-basic1">
+                <span class="menu-title">Dietitian</span>
+                <i class="icon-people menu-icon"></i>
+              </a>
+              <div class="collapse" id="ui-basic1">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="adddietitian.php">Add Dietitian</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="viewdietion.php">View Dietitian</a></li>
+                </ul>
+              </div>
+            </li>
+			      <!-- <li class="nav-item">
+              <a class="nav-link" href="viewrequestplans.php">
+                <span class="menu-title">New Diet-Plan Requests</span>
+                <i class="icon-notebook menu-icon"></i>
+              </a>
+            </li> -->
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+                <span class="menu-title">Health Classes</span>
+                <i class="icon-doc menu-icon"></i>
+              </a>
+              <div class="collapse" id="auth">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="addcounselling.php"> Add Classes </a></li>
+                  <li class="nav-item"> <a class="nav-link" href="viewprograms.php"> View Classes </a></li>
+                </ul>
+              </div>
+            </li>
+			
+               <li class="nav-item">
+              <a class="nav-link" href="addcategory.php">
+                <span class="menu-title">Add Category</span>
+                <i class="icon-notebook menu-icon"></i>
+              </a>
+            </li>
+			      <li class="nav-item">
+              <a class="nav-link" href="viewcategory.php">
+                <span class="menu-title">View Category</span>
+                <i class="icon-notebook menu-icon"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="collapse" href="#auth1" aria-expanded="false" aria-controls="auth">
+                <span class="menu-title">Trainers</span>
+                <i class="icon-people menu-icon"></i>
+              </a>
+              <div class="collapse" id="auth1">
+                <ul class="nav flex-column sub-menu">
+                  <li class="nav-item"> <a class="nav-link" href="addtrainers.php"> Add Trainers</a></li>
+                  <li class="nav-item"> <a class="nav-link" href="viewtrainers.php"> View Trainers </a></li>
+                </ul>
+              </div>
+			      </li>
+            <li class="nav-item">
+              <a class="nav-link" href="viewtraining.php">
+                <span class="menu-title">Workout/Yoga Programs</span>
+                <i class="icon-doc menu-icon"></i>
+              </a>
+            </li>
+            </li>
+          </ul>
+        </nav>
